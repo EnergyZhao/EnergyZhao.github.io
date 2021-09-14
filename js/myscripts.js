f501e72884e32b44cd96cd106d1127562b6efdad
Bo Zhao <bzhao35@gatech.edu>
2021-09-13 23:46:33 -0500

    Update myscripts.js

--- js/myscripts.js
+++ js/myscripts.js
@@ -25,7 +25,7 @@ document.getElementById("myFooter").innerHTML =
 	+"target='_blank'>Department of Mechanical Engineering</a></li>"
 	+"<li>Cullen College of Engineering</li>"
 	+"<li>University of Houston</li>"
-	+"<li>Office: to be announced</li>"
+	+"<li>Office: N234, Engineering Building 1</li>"
 	+"<li>Email: bzhao8-at-uh-dot-edu</li>"
 	+"</ul>"
 	+"</div>"
